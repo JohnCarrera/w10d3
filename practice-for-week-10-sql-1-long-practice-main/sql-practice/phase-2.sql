6cb836f0b48822791f77ae2a8ab386fb6e2fa08e
JohnCarrera <johnrcarrera@gmail.com>
2022-08-10 16:41:35 -0400

    mid-day

--- practice-for-week-10-sql-1-long-practice-main/sql-practice/phase-2.sql
+++ practice-for-week-10-sql-1-long-practice-main/sql-practice/phase-2.sql
@@ -1,1 +1,21 @@
--- Your code here
+CREATE TABLE customers (
+    id      INTEGER PRIMARY KEY
+  , name    VARCHAR(40) NOT NULL
+  , phone   NUMERIC (10, 0) UNIQUE
+  , email   VARCHAR(255) UNIQUE
+  , points  INTEGER NOT NULL DEFAULT 5
+  , created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE coffee_orders (
+  id INTEGER PRIMARY KEY
+  , is_redeemed BOOLEAN DEFAULT false
+  , ordered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+UPDATE customers
+SET points = (
+  SELECT points
+  FROM customers AS c
+  WHERE c.name = 'Rachel') + 1
+WHERE name = 'Rachel';
